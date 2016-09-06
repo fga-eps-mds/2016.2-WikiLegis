8358c5de0c50b3dd39f9a310be68ac465b3305ef
Thiago Nogueira <thiagonf10@gmail.com>
2016-09-06 15:58:29 -0300

    Applying style and design on User

--- app/src/main/java/gppmds/wikilegis/model/User.java
+++ app/src/main/java/gppmds/wikilegis/model/User.java
@@ -16,27 +16,37 @@ public class User {
     private static final int MAX_LENGTH_PASSWORD = 10;
     private static final int MIN_LENGTH_PASSWORD = 6;
 
-    public static final String FIRST_NAME_CANT_BE_EMPTY = "Inválido, o nome não pode ser vazio.";
-    public static final String FIRST_NAME_CANT_BE_HIGHER_THAN_30 = "Inválido, o nome deve ter no máximo 30 caractéres";
-    public static final String LAST_NAME_CANT_BE_EMPTY = "Inválido, o sobrenome não pode ser vazio.";
-    public static final String LAST_NAME_CANT_BE_HIGHER_THAN_30 = "Inválido, o sobrenome deve ter no máximo 30 caractéres";
-    public static final String EMAIL_CANT_BE_EMPTY_EMAIL = "Inválido, o email não pode ser vazio.";
-    public static final String EMAIL_CANT_BE_HIGHER_THAN_150 = "Inválido, o email deve ter no máximo 30 caractéres";
+    public static final String FIRST_NAME_CANT_BE_EMPTY = "Inválido, o nome "
+            + "não pode ser vazio.";
+    public static final String FIRST_NAME_CANT_BE_HIGHER_THAN_30 = "Inválido, "
+            + "o nome deve ter no máximo 30 caractéres";
+    public static final String LAST_NAME_CANT_BE_EMPTY = "Inválido, o sobrenome "
+            + "não pode ser vazio.";
+    public static final String LAST_NAME_CANT_BE_HIGHER_THAN_30 = "Inválido, "
+            + "o sobrenome deve ter no máximo 30 caractéres";
+    public static final String EMAIL_CANT_BE_EMPTY_EMAIL = "Inválido, o email "
+            + "não pode ser vazio.";
+    public static final String EMAIL_CANT_BE_HIGHER_THAN_150 = "Inválido, o "
+            + "email deve ter no máximo 30 caractéres";
     public static final String INVALID_EMAIL = "Ops, esse e-mail é inválido.";
-    public static final String PASSWORD_CANT_BE_EMPTY = "Inválido, a senha não pode ser vazia.";
-    public static final String PASSWORD_CANT_BE_LESS_THAN_6 = "Inválido, a senha deve conter no mínimo 6 caractéres.";
-    public static final String PASSWORD_CANT_BE_HIGHER_THAN_10 = "Inválido, a senha deve ter no máximo 10 caractéres";
+    public static final String PASSWORD_CANT_BE_EMPTY = "Inválido, a senha não "
+            + "pode ser vazia.";
+    public static final String PASSWORD_CANT_BE_LESS_THAN_6 = "Inválido, a senha"
+            + " deve conter no mínimo 6 caractéres.";
+    public static final String PASSWORD_CANT_BE_HIGHER_THAN_10 = "Inválido, a senha"
+            + " deve ter no máximo 10 caractéres";
     public static final String PASSWORD_ISNT_EQUALS = "As senhas digitadas sao diferentes";
     public static final String EMAIL_IS_REPEATED = "Este email já está cadastrado";
     public static final String NAME_CONTAINS_SPECIAL_CHARACTERS = "O nome deve ter apenas letras.";
-    public static final String LAST_NAME_CONTAINS_SPECIAL_CHARACTERS = "O sobrenome deve ter apenas letras.";
+    public static final String LAST_NAME_CONTAINS_SPECIAL_CHARACTERS = "O sobrenome"
+            + " deve ter apenas letras.";
 
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
-    public User(String firstName , String lastName, String email, String password,
+    public User(String firstName, String lastName, String email, String password,
                 String passwordConfimation) throws UserException {
         setFirstName(firstName);
         setLastName(lastName);
@@ -73,43 +83,37 @@ public class User {
     }
 
     private void setLastName(String lastName) throws UserException {
-        if(  stringIsNull(lastName)){
-            if(  validateNameContainsOnlyLetters(lastName)) {
-
-
+        if (stringIsNull(lastName)) {
+            if (validateNameContainsOnlyLetters(lastName)) {
                 if (validateStringLengthLessThanMax(lastName, MAX_LENGTH_LAST_NAME)) {
                     this.lastName = lastName;
                 } else {
                     throw new UserException(LAST_NAME_CANT_BE_HIGHER_THAN_30);
                 }
-            } else{
+            } else {
                throw new UserException(LAST_NAME_CONTAINS_SPECIAL_CHARACTERS);
             }
-        }
-        else {
+        } else {
             throw  new UserException(LAST_NAME_CANT_BE_EMPTY);
         }
     }
 
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
     private void setEmail(String email) throws UserException {
         if (stringIsNull(email)) {
-
-            if(  validateStringLengthLessThanMax(email, MAX_LENGTH_EMAIL)){
-
-                if(  validateEmailFormat(email)) {
+            if (validateStringLengthLessThanMax(email, MAX_LENGTH_EMAIL)) {
+                if (validateEmailFormat(email)) {
                         this.email = email;
-                }
-                else{
+                } else {
                     throw new UserException(INVALID_EMAIL);
                 }
-            }
-            else{
+            } else {
                 throw  new UserException(EMAIL_CANT_BE_HIGHER_THAN_150);
             }
-        }
-        else{
+        } else {
             throw  new UserException(EMAIL_CANT_BE_EMPTY_EMAIL);
         }
     }
@@ -118,16 +122,16 @@ public class User {
         return password;
     }
 
-    private void setPassword( String password, String passwordConfirmation) throws UserException {
-        if(  stringIsNull(password)){
-            if(  validateStringLengthLessThanMax(password, MAX_LENGTH_PASSWORD)){
-                if(  validateStringLengthMoreThanMin(password, MIN_LENGTH_PASSWORD)){
-                    if(  passwordIsEqual(password, passwordConfirmation)){
+    private void setPassword(String password, String passwordConfirmation) throws UserException {
+        if (stringIsNull(password)) {
+            if (validateStringLengthLessThanMax(password, MAX_LENGTH_PASSWORD)) {
+                if (validateStringLengthMoreThanMin(password, MIN_LENGTH_PASSWORD)) {
+                    if (passwordIsEqual(password, passwordConfirmation)) {
                         this.password = password;
                     } else {
                         throw new UserException(PASSWORD_ISNT_EQUALS);
                     }
-                }else {
+                } else {
                     throw  new UserException(PASSWORD_CANT_BE_LESS_THAN_6);
                 }
             } else {
@@ -140,8 +144,8 @@ public class User {
 
     //Validation methods
 
-    private boolean stringIsNull(final String string){
-        if(  string == null || string.trim().isEmpty()) {
+    private boolean stringIsNull(final String string) {
+        if (string == null || string.isEmpty()) {
             return false;
         } else {
             return  true;
@@ -149,16 +153,16 @@ public class User {
     }
 
 
-    private boolean validateStringLengthLessThanMax(final String string, final int MAX){
-        if(  string.length() > MAX) {
+    private boolean validateStringLengthLessThanMax(final String string, final int MAX) {
+        if (string.length() > MAX) {
             return false;
         } else {
             return  true;
         }
     }
 
-    private boolean validateStringLengthMoreThanMin(final String string, final int MIN){
-        if(  string.length() < MIN) {
+    private boolean validateStringLengthMoreThanMin(final String string, final int MIN) {
+        if (string.length() < MIN) {
             return false;
         } else {
             return  true;
@@ -166,9 +170,9 @@ public class User {
     }
 
 
-    private boolean passwordIsEqual(final String password, final String passwordConfimation){
+    private boolean passwordIsEqual(final String password, final String passwordConfimation) {
 
-        if(  password.equals(passwordConfimation)) {
+        if (password.equals(passwordConfimation)) {
             return true;
         } else {
             return  false;
@@ -176,7 +180,7 @@ public class User {
     }
 
     //Validar tipo de email
-    private boolean validateEmailFormat (final String email) {
+    private boolean validateEmailFormat(final String email) {
         String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
         CharSequence inputStr = email;
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
@@ -188,12 +192,10 @@ public class User {
         }
     }
 
-    private boolean validateNameContainsOnlyLetters(final String name){
-
+    private boolean validateNameContainsOnlyLetters(final String name) {
         for (int i = 0; i < name.length(); i++) {
             if (Character.isLetter(name.charAt(i)) == false) {
                 return false;
-
             }
         }
         return true;
