f15c70a687e6a62dd2c6fc66d6c2c9cac060193d
Thiago Nogueira <thiagonf10@gmail.com>
2016-09-06 22:10:26 -0300

    Creating Bill class and your constructor

---
+++ app/src/main/java/gppmds/wikilegis/model/Bill.java
@@ -0,0 +1,20 @@
+package gppmds.wikilegis.model;
+
+/**
+ * Created by thiago on 9/6/16.
+ */
+public class Bill {
+    private Integer id;
+    private String title;
+    private String name;
+    private String epigraph;
+    private String status;
+    private String theme;
+    private String segments;
+    private String numberOfLike;
+    private String numberOfDislike;
+
+    public Bill(final Integer id, final String title, final String name, final String epigraph, final String status, final String theme, final String segments, final String numberOfLike, final String numberOfDislike) {
+    
+    }
+}
