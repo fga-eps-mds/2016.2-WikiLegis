329b370748a3f4cc04ced2fb175296cb709c9901
Thiago Nogueira <thiagonf10@gmail.com>
2016-09-06 22:25:55 -0300

    Adding gets and sets the Bill class

--- app/src/main/java/gppmds/wikilegis/model/Bill.java
+++ app/src/main/java/gppmds/wikilegis/model/Bill.java
@@ -15,6 +15,87 @@ public class Bill {
     private String numberOfDislike;
 
     public Bill(final Integer id, final String title, final String name, final String epigraph, final String status, final String theme, final String segments, final String numberOfLike, final String numberOfDislike) {
-    
+        setId(id);
+        setTitle(title);
+        setName(name);
+        setEpigraph(epigraph);
+        setStatus(status);
+        setTheme(theme);
+        setSegments(segments);
+        setNumberOfLike(numberOfLike);
+        setNumberOfDislike(numberOfDislike);
     }
-}
+
+    public Integer getId() {
+        return id;
+    }
+
+    private void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEpigraph() {
+        return epigraph;
+    }
+
+    private void setEpigraph(String epigraph) {
+        this.epigraph = epigraph;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    private void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    private void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getSegments() {
+        return segments;
+    }
+
+    private void setSegments(String segments) {
+        this.segments = segments;
+    }
+
+    public String getNumberOfLike() {
+        return numberOfLike;
+    }
+
+    private void setNumberOfLike(String numberOfLike) {
+        this.numberOfLike = numberOfLike;
+    }
+
+    public String getNumberOfDislike() {
+        return numberOfDislike;
+    }
+
+    private void setNumberOfDislike(String numberOfDislike) {
+        this.numberOfDislike = numberOfDislike;
+    }
+
+    }
