b057b9ed0c146e4b6545d6b5c3fa2e0f81d7020d
Izabela Cardoso <cizabelacristina@gmail.com>
2016-09-08 08:32:19 -0300

    Changing test to work on Cricle CI

--- app/src/androidTest/java/gppmds/wikilegis/view/RegisterUserFragmentTest.java
+++ app/src/androidTest/java/gppmds/wikilegis/view/RegisterUserFragmentTest.java
@@ -114,6 +114,7 @@ public class RegisterUserFragmentTest extends ActivityInstrumentationTestCase2<R
     public void testErrorWithOverMaxLengthPassword(){
         onView(withId(R.id.firstNameField)).perform(typeText("aaaaa"));
         onView(withId(R.id.lastNameField)).perform(typeText("aaaaaa"));
+        closeSoftKeyboard();
         onView(withId(R.id.emailField)).perform(typeText("aaaaa@gmail.com"));
         onView(withId(R.id.passwordField)).perform(typeText("1234567891011"));
         closeSoftKeyboard();
