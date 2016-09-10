f48811ce49e0df6082faf8b02301b00543def8c8
Marcelo Augusto <mekmay@hotmail.com>
2016-09-09 21:18:52 -0300

    Adding SegmentTypesException and SegmentTypes validation

--- app/src/main/java/gppmds/wikilegis/model/SegmentTypes.java
+++ app/src/main/java/gppmds/wikilegis/model/SegmentTypes.java
@@ -5,6 +5,10 @@ package gppmds.wikilegis.model;
  */
 public class SegmentTypes {
 
+    public static final String NAME_CANT_BE_EMPTY = "Nome não pode ser carregado.";
+    public static final String ID_CANT_BE_EMPTY = "Id não pode ser carregado.";
+    public static final String ID_CANT_BE_LESS_THAN_1 = "Id não pode ser carregado.";
+
     private Integer id;
     private String name;
 
@@ -22,10 +26,31 @@ public class SegmentTypes {
     }
 
     public void setId(final Integer id) {
-        this.id=id;
+        if(idIsValid(id) || !idIsNull(id)) {
+            this.id=id;
+        } else {
+            //fazer a exceção
+        }
     }
 
     public void setName(final String name) {
-        this.name=name;
+        if(!nameIsNull(name)) {
+            this.name=name;
+        } else {
+            //fazer a exceção
+        }
     }
+
+    private boolean idIsNull(final Integer id) {
+        return id == null ? true : false;
+    }
+
+    private boolean idIsValid(final Integer id) {
+        return id < 1 ? false : true;
+    }
+
+    private boolean nameIsNull(final String name) {
+        return (name.isEmpty() || name == null) ? true : false;
+    }
+
 }
