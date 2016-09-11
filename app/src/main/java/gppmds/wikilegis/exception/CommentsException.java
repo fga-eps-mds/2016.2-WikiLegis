36faeb5cfe2e21d001212bcd1ae0e7ebec175802
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-11 19:41:24 -0300

    Adding Comments Exception class

---
+++ app/src/main/java/gppmds/wikilegis/exception/CommentsException.java
@@ -0,0 +1,10 @@
+package gppmds.wikilegis.exception;
+
+/**
+ * Created by augusto on 11/09/16.
+ */
+public class CommentsException {
+    public CommentsException(final String message) {
+        super(message);
+    }
+}
--- app/src/main/java/gppmds/wikilegis/model/Comments.java
+++ app/src/main/java/gppmds/wikilegis/model/Comments.java
@@ -89,4 +89,4 @@ public class Comments {
     }
 }
 
-}
+
