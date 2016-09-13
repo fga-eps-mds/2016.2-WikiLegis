81f6bc422108ab5cde45ba65c9d3913d862321e4
Marcelo Augusto <mekmay@hotmail.com>
2016-09-13 19:13:08 -0300

    Create methods isDatabaseEmpty and insertBill in BillDao

--- app/src/main/java/gppmds/wikilegis/dao/BillDAO.java
+++ app/src/main/java/gppmds/wikilegis/dao/BillDAO.java
@@ -1,6 +1,11 @@
 package gppmds.wikilegis.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import gppmds.wikilegis.model.Bill;
 
 /**
  * Created by marcelo on 9/13/16.
@@ -26,5 +31,53 @@ public class BillDAO extends DaoUtilities{
         }
         return BillDAO.instance;
     }
+
+    public boolean  isDatabaseEmpty(){
+        sqliteDatabase = database.getReadableDatabase();
+
+        String query = "SELECT 1 FROM " + tableName;
+
+        Cursor cursor = sqliteDatabase.rawQuery(query, null);
+
+        boolean isEmpty = false;
+
+        if (cursor != null) {
+            if (cursor.getCount() <= 0) {
+                cursor.moveToFirst();
+                isEmpty = true;
+
+            } else {
+                //nothing to do
+            }
+
+        } else {
+
+            isEmpty = true;
+        }
+
+        return isEmpty;
+    }
+
+    public boolean insertBill(Bill bill) {
+
+        SQLiteDatabase sqLiteDatabase = database.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(tableColumns[0], bill.getId());
+        values.put(tableColumns[1], bill.getTitle());
+        values.put(tableColumns[2], bill.getEpigraph());
+        values.put(tableColumns[3], bill.getDescription());
+        values.put(tableColumns[4], bill.getTheme());
+        //values.put(tableColumns[5], bill.getAmountParticipants());
+        //values.put(tableColumns[7], bill.getAmountProposals());
+        values.put(tableColumns[7], bill.getStatus());
+
+
+        boolean result = insertAndClose(sqLiteDatabase, tableName, values) > 0;
+
+        return result;
+    }
+
 
 }
