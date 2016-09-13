28ce650633cb15d2b172a12c27f167a16547a11f
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-13 18:05:16 -0300

    Adding variables to vote class

--- app/src/main/java/gppmds/wikilegis/model/Votes.java
+++ app/src/main/java/gppmds/wikilegis/model/Votes.java
@@ -4,4 +4,10 @@ package gppmds.wikilegis.model;
  * Created by augusto on 13/09/16.
  */
 public class Votes {
+
+    private Integer userId;
+    private Integer contentType;
+    private Integer objectId;
+    private boolean vote;   
+
 }
