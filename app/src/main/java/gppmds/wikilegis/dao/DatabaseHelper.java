4ee6057863cd69b3cade56a7cce2dff57a9220cc
Marcelo Augusto <mekmay@hotmail.com>
2016-09-13 17:25:08 -0300

    Adding database tables of Bill and Segments of Bill

--- app/src/main/java/gppmds/wikilegis/dao/DatabaseHelper.java
+++ app/src/main/java/gppmds/wikilegis/dao/DatabaseHelper.java
@@ -13,7 +13,35 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     private static final Integer DATABASE_VERSION = 1;
 
     private static final String BILL_TABLE = "[Bill]";
+    private static final String SEGMENTS_BILL_TABLE = "[SegmentsBill]";
 
+    //Bill
+
+    private static final String BILL_ID = "[id]";
+    private static final String BILL_TITLE = "[title]";
+    private static final String BILL_EPIGRAPH = "[epigraph]";
+    private static final String BILL_DESCRIPTION = "[description]";
+    private static final String BILL_THEME = "[theme]";
+    private static final String BILL_AMOUNT_PARTICIPANTS = "[amountParticipants]";
+    private static final String BILL_AMOUNT_PROPOSALS = "[amountProposals]";
+    private static final String BILL_STATUS = "[status]";
+
+
+    //Segments of Bill
+
+    private static final String SEGMENTS_BILL_ID_SEGMENT = "[idSegment]";
+    private static final String SEGMENTS_BILL_ID_BILL = "[idBill]";
+
+    private static final String CREATE_BILL = "CREATE TABLE " + BILL_TABLE + " (" +
+            BILL_ID + " INTEGER NOT NULL PRIMARY KEY, " + BILL_TITLE + " VARCHAR(200), " +
+            BILL_EPIGRAPH + "  VARCHAR(200), "+ BILL_DESCRIPTION + " VARCHAR(500), " +
+            BILL_THEME + " VARCHAR(50), "+ BILL_AMOUNT_PARTICIPANTS + " INTEGER, " +
+            BILL_AMOUNT_PROPOSALS + " INTEGER, " + BILL_STATUS + " VARCHAR(50)); ";
+
+
+    private static final String CREATE_SEGMENTS_BILL = "CREATE TABLE" + SEGMENTS_BILL_TABLE + " ( " +
+            SEGMENTS_BILL_ID_SEGMENT + " INTEGER NOT NULL PRIMARY KEY, " + SEGMENTS_BILL_ID_BILL +
+            " INTEGER );";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,6 +49,8 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BILL);
+        db.execSQL(CREATE_SEGMENTS_BILL);
     }
 
     @Override
