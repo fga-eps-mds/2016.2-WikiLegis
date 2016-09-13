0f1b56fdc5454db37aff3d5744ed0d07fbd7dfdf
Marcelo Augusto <mekmay@hotmail.com>
2016-09-13 18:40:12 -0300

    Create BillDAO and contructors

--- app/src/main/java/gppmds/wikilegis/dao/BillDAO.java
+++ app/src/main/java/gppmds/wikilegis/dao/BillDAO.java
@@ -1,9 +1,30 @@
 package gppmds.wikilegis.dao;
 
+import android.content.Context;
+
 /**
  * Created by marcelo on 9/13/16.
  */
-public class BillDAO {
+public class BillDAO extends DaoUtilities{
 
+    private static String tableColumns[] = {"id", "title", "epigraph", "description", "theme",
+    "amountParticipants", "amountProposals", "status"};
+
+    private static BillDAO instance;
+
+    private static String tableName = "Bill";
+
+    private BillDAO(Context context) {
+        BillDAO.database = new DatabaseHelper(context);
+    }
+
+    public static BillDAO getInstance(Context context) {
+        if (BillDAO.instance != null) {
+            //nothing to do
+        } else {
+            BillDAO.instance = new BillDAO(context);
+        }
+        return BillDAO.instance;
+    }
 
 }
