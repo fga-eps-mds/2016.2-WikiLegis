46a6fcbea4b858f13ae61f8a5d72e95a3763c593
Marcelo Augusto <mekmay@hotmail.com>
2016-09-13 20:10:16 -0300

    Adding method to init all segments and bill in database

--- app/src/main/java/gppmds/wikilegis/controller/BillController.java
+++ app/src/main/java/gppmds/wikilegis/controller/BillController.java
@@ -2,9 +2,16 @@ package gppmds.wikilegis.controller;
 
 import android.util.Log;
 
+import org.json.JSONException;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import gppmds.wikilegis.dao.BillDAO;
+import gppmds.wikilegis.dao.JSONHelper;
+import gppmds.wikilegis.exception.BillException;
+import gppmds.wikilegis.exception.SegmentException;
+import gppmds.wikilegis.model.Bill;
 import gppmds.wikilegis.model.Segment;
 
 /**
@@ -12,23 +19,37 @@ import gppmds.wikilegis.model.Segment;
  */
 public class BillController {
 
+    private static List<Bill> billList = new ArrayList<Bill>();
+    private static BillDAO billDao;
 
-    public static int countedTheNumberOfProposals(List<Segment> segmentList , int id){
+    public List<Bill> getAllBills(){
+        return billList;
+    }
+
+    public void initControllerBills() throws BillException, JSONException, SegmentException {
+        if (billDao.isDatabaseEmpty()) {
+
+            billList = JSONHelper.billListFromJSON(JSONHelper.getJSONObjectApi("http://wikilegis.labhackercd.net/api/bills/"),
+                    SegmentController.getAllSegments());
+        } else {
+            billList = billDao.getAllBills();
+        }
+    }
+
+    public static int countedTheNumberOfProposals(List<Segment> segmentList, int id){
 
         int counter = 0;
 
-        for(int index = 0 ;index<segmentList.size() ;index ++){
+        for (int index = 0; index < segmentList.size(); index++) {
 
-                if(segmentList.get(index).getBill() == id){
-
-                    if(segmentList.get(index).getReplaced()!=0){
-                            counter ++;
-                    }
+            if (segmentList.get(index).getBill() == id) {
+                if (segmentList.get(index).getReplaced() != 0) {
+                        counter++;
+                }
             }
-
         }
-
         return counter;
     }
+
 
 }
---
+++ app/src/main/java/gppmds/wikilegis/controller/SegmentController.java
@@ -0,0 +1,33 @@
+package gppmds.wikilegis.controller;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gppmds.wikilegis.dao.JSONHelper;
+import gppmds.wikilegis.dao.SegmentDAO;
+import gppmds.wikilegis.exception.SegmentException;
+import gppmds.wikilegis.model.Segment;
+
+/**
+ * Created by marcelo on 9/13/16.
+ */
+public class SegmentController {
+
+    private static List<Segment> segmentList = new ArrayList<Segment>();
+    private static SegmentDAO segmentDAO;
+
+    public static List<Segment> getAllSegments(){
+        return segmentList;
+    }
+
+    public void initControllerSegments() throws SegmentException, JSONException {
+        if (segmentDAO.isDatabaseEmpty()) {
+            segmentList = JSONHelper.segmentListFromJSON();
+        } else {
+            segmentList = segmentDAO.getAllSegments();
+        }
+    }
+
+}
