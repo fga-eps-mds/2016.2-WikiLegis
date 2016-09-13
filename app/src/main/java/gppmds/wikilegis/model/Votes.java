abf53a3f6bee30270f0637a8202ad8a422d70fab
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-13 18:07:45 -0300

    Adding getters and setters to Vote class

--- app/src/main/java/gppmds/wikilegis/model/Votes.java
+++ app/src/main/java/gppmds/wikilegis/model/Votes.java
@@ -8,6 +8,37 @@ public class Votes {
     private Integer userId;
     private Integer contentType;
     private Integer objectId;
-    private boolean vote;   
+    private boolean vote;
 
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(Integer contentType) {
+        this.contentType = contentType;
+    }
+
+    public Integer getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(Integer objectId) {
+        this.objectId = objectId;
+    }
+
+    public boolean isVote() {
+        return vote;
+    }
+
+    public void setVote(boolean vote) {
+        this.vote = vote;
+    }
 }
