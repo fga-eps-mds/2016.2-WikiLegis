a7aef5d960e817df51e34f529bddda3121e3fb27
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-18 15:56:01 -0300

    Adding idAuthor tests to SegmentTest class

--- app/src/test/java/gppmds/wikilegis/model/SegmentTest.java
+++ app/src/test/java/gppmds/wikilegis/model/SegmentTest.java
@@ -137,4 +137,19 @@ public class SegmentTest {
 
         assertFalse(isValid);
     }
+
+    @Test
+
+    public void testNullIdAuthor(){
+        boolean isValid = true;
+
+        try{
+            Segment segment = new Segment(1,2,3,true,4,5,6,10,"Comment",null,8,9);
+        }catch (SegmentException segmentException){
+            isValid = false;
+        }
+
+        assertFalse(isValid);
+    }
 }
+
