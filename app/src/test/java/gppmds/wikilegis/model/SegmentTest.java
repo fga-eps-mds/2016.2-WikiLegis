f1a27fa4cf043b62029fc1cac70f032af5d4c2a9
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-18 15:30:04 -0300

    Adding Id Tests to SegmentTest class

--- app/src/test/java/gppmds/wikilegis/model/SegmentTest.java
+++ app/src/test/java/gppmds/wikilegis/model/SegmentTest.java
@@ -1,7 +1,27 @@
 package gppmds.wikilegis.model;
 
+import org.junit.Test;
+
+import gppmds.wikilegis.exception.SegmentException;
+
+import static junit.framework.Assert.assertFalse;
+
 /**
  * Created by augusto on 18/09/16.
  */
 public class SegmentTest {
+
+    @Test
+
+    public void testNullId(){
+        boolean isValid = true;
+
+        try{
+            Segment segment = new Segment(null,1,2,true,3,4,5,6,"content",7,8,9);
+        }catch (SegmentException segmentException){
+            isValid = false;
+        }
+
+        assertFalse(isValid);
+    }
 }
