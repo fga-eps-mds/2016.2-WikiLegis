3cb8945c2b8879865f1229829a2708b528d52aff
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-18 16:10:40 -0300

    Adding validations to set methord at SegmentsOfBills class

--- app/src/main/java/gppmds/wikilegis/model/SegmentsOfBill.java
+++ app/src/main/java/gppmds/wikilegis/model/SegmentsOfBill.java
@@ -1,15 +1,22 @@
 package gppmds.wikilegis.model;
 
+import gppmds.wikilegis.exception.SegmentException;
+
 /**
  * Created by marcelo on 9/16/16.
  */
 public class SegmentsOfBill {
 
+    private static final String IDBILL_CANT_BE_NULL = "idBill não pode ser carregado";
+    private static final String IDSEGMENT_CANT_BE_NULL = "idBill não pode ser carregado";
+    private static final String POSITION_CANT_BE_NULL = "idBill não pode ser carregado";
+
+
     private Integer idSegment;
     private Integer idBill;
     private Integer position;
 
-    public SegmentsOfBill(Integer idBill, Integer idSegment, Integer position) {
+    public SegmentsOfBill(Integer idBill, Integer idSegment, Integer position) throws SegmentException {
         setIdBill(idBill);
         setIdSegment(idSegment);
         setPosition(position);
@@ -19,25 +26,39 @@ public class SegmentsOfBill {
         return idBill;
     }
 
-    private void setIdBill(Integer idBill) {
+    private void setIdBill(Integer idBill) throws SegmentException {
 
-        this.idBill=idBill;
+        if(validateIntegerNull(idBill)) {
+            this.idBill = idBill;
+        } else {
+            throw new SegmentException(IDBILL_CANT_BE_NULL);
+        }
     }
 
     public Integer getIdSegment() {
         return idSegment;
     }
 
-    private void setIdSegment(Integer idSegment) {
-        this.idSegment=idSegment;
+    private void setIdSegment(Integer idSegment) throws SegmentException {
+
+        if(validateIntegerNull(idSegment)) {
+            this.idSegment = idSegment;
+        } else {
+            throw new SegmentException(IDSEGMENT_CANT_BE_NULL);
+        }
     }
 
     public Integer getPosition() {
         return position;
     }
 
-    private void setPosition(Integer position) {
-        this.position=position;
+    private void setPosition(Integer position) throws SegmentException {
+
+        if(validateIntegerNull(position)) {
+            this.position = position;
+        } else {
+                throw new SegmentException(POSITION_CANT_BE_NULL);
+        }
     }
 
 
