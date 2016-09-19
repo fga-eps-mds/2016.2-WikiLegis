e00433e12e828e20c197d6670530fe6305e5f427
Marcelo Augusto <mekmay@hotmail.com>
2016-09-19 01:19:37 -0300

    Adding check and filtering for Recents/Relevantes, Open/Closed in listView

--- app/src/main/java/gppmds/wikilegis/view/RecyclerViewAdapter.java
+++ app/src/main/java/gppmds/wikilegis/view/RecyclerViewAdapter.java
@@ -103,7 +103,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(BillViewHolder personViewHolder, int i) {
-        bills = MainActivity.filtringForNumberOfProposals(bills);
+        bills = MainActivity.filteringForNumberOfProposals(bills);
         personViewHolder.billTitle.setText(bills.get(i).getTitle());
         personViewHolder.billDescription.setText(bills.get(i).getDescription());
         personViewHolder.billNumberProposal.setText(Integer.toString(bills.get(i).getNumberOfPrposals()));
