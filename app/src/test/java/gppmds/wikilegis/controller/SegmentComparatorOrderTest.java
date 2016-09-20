b306132be87bd1f4da3a4fe22bd70b96a6b97821
Augusto Vilarins <augusto.vilarins@gmail.com>
2016-09-20 13:27:14 -0300

    Adding SegmentComparatorOrder tests

---
+++ app/src/test/java/gppmds/wikilegis/controller/SegmentComparatorOrderTest.java
@@ -0,0 +1,82 @@
+package gppmds.wikilegis.controller;
+
+import org.junit.Test;
+
+import gppmds.wikilegis.exception.SegmentException;
+import gppmds.wikilegis.model.Segment;
+
+import static junit.framework.Assert.assertEquals;
+
+
+/**
+ * Created by augusto on 20/09/16.
+ */
+public class SegmentComparatorOrderTest {
+
+    @Test
+
+    public void testHigherOrder() {
+        int count = 0;
+
+        SegmentComparatorOrder segmentComparatorOrder = new SegmentComparatorOrder();
+
+
+        try {
+            Segment segment = new Segment(1,3, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+            Segment segment2 = new Segment(1,2, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+
+            count = segmentComparatorOrder.compare(segment, segment2);
+
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(count, 1);
+
+    }
+
+    @Test
+
+    public void testLowerOrder(){
+        int count = 0;
+
+        SegmentComparatorOrder segmentComparatorOrder = new SegmentComparatorOrder();
+
+
+        try {
+            Segment segment = new Segment(1,1, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+            Segment segment2 = new Segment(1,2, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+
+            count = segmentComparatorOrder.compare(segment, segment2);
+
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(count, -1);
+
+    }
+
+    @Test
+
+    public void testEqualOrder(){
+        int count = 0;
+
+        SegmentComparatorOrder segmentComparatorOrder = new SegmentComparatorOrder();
+
+
+        try {
+            Segment segment = new Segment(1,2, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+            Segment segment2 = new Segment(1,2, 3, true, 4, 5, 6, 10, "Comment", 8, 10, 9, "Date");
+
+            count = segmentComparatorOrder.compare(segment, segment2);
+
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(count,0);
+
+    }
+
+}
