d55f4ec459bd8782e01adf0e1dfbc718e7cf94f0
Thiago Nogueira <thiagonf10@gmail.com>
2016-09-20 17:11:41 -0300

    Applying chek style in BillComparatorProposal and FilteringController

--- app/src/main/java/gppmds/wikilegis/controller/FilteringController.java
+++ app/src/main/java/gppmds/wikilegis/controller/FilteringController.java
@@ -1,64 +1,58 @@
 package gppmds.wikilegis.controller;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import gppmds.wikilegis.model.Bill;
 
-/**
- * Created by josue on 9/9/16.
- */
 public class FilteringController {
 
     private Context context;
     private static FilteringController instance = null;
 
-    private FilteringController(Context context) {
+    private FilteringController(final Context context) {
         this.context = context;
     }
 
-    public static FilteringController getInstance(Context context){
-        if(instance == null){
+    public static FilteringController getInstance(final Context context) {
+        if (instance == null) {
             instance = new FilteringController(context);
         }
         return  instance;
     }
 
-    public List<Bill> filterigForStatusClosed(List<Bill> list){
-        List<Bill> billListWithStatusClosed= new ArrayList<Bill>();
+    public List<Bill> filterigForStatusClosed(final List<Bill> list) {
+        List<Bill> billListWithStatusClosed = new ArrayList<Bill>();
 
-        for(int index = 0 ; index<list.size();index++){
-            if(list.get(index).getStatus().equals("closed")){
+        for (int index = 0; index < list.size(); index++) {
+            if (list.get(index).getStatus().equals("closed")) {
                 billListWithStatusClosed.add(list.get(index));
             }
         }
         return billListWithStatusClosed;
     }
 
-    public List<Bill> filterigForStatusPublished(List<Bill> list){
-        List<Bill> billListWithStatusPublished= new ArrayList<Bill>();
+    public List<Bill> filterigForStatusPublished(final List<Bill> list) {
+        List<Bill> billListWithStatusPublished = new ArrayList<Bill>();
 
 
-        for(int index = 0 ; index<list.size();index++){
-            if(list.get(index).getStatus().equals("published")){
+        for (int index = 0; index < list.size(); index++) {
+            if (list.get(index).getStatus().equals("published")) {
                 billListWithStatusPublished.add(list.get(index));
             }
         }
         return billListWithStatusPublished;
     }
 
-    public static List<Bill> filteringForNumberOfProposals(List<Bill> billList) {
+    public static List<Bill> filteringForNumberOfProposals(final List<Bill> billList) {
         List<Bill> billListAux = new ArrayList<>();
 
         BillComparatorProposals billComparatorDProposals = new BillComparatorProposals();
         Collections.sort(billList, billComparatorDProposals);
 
-        for(int i=0; i<billList.size(); i++) {
+        for (int i = 0; i < billList.size(); i++) {
             billListAux.add(billList.get(i));
         }
 
@@ -66,13 +60,13 @@ public class FilteringController {
 
     }
 
-    public List<Bill> filteringForDate(List<Bill> billList){
+    public List<Bill> filteringForDate(final List<Bill> billList) {
         List<Bill> billListAux = new ArrayList<>();
 
         BillComparatorDate comparator = new BillComparatorDate();
-        Collections.sort(billList,comparator);
+        Collections.sort(billList, comparator);
 
-        for(int i=0; i<billList.size(); i++) {
+        for (int i = 0; i < billList.size(); i++) {
             billListAux.add(billList.get(i));
         }
 
