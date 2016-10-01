1798e71f20d07577e808ce93b7b594547ed9fab5
rodrigocam <rodrigo.redcode@gmail.com>
2016-09-30 22:54:38 -0300

    Adding tests of getAllSegments and getSegmentById of SegmentController

---
+++ app/src/androidTest/java/gppmds/wikilegis/controller/SegmentControllerTest.java
@@ -0,0 +1,65 @@
+package gppmds.wikilegis.controller;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gppmds.wikilegis.exception.SegmentException;
+import gppmds.wikilegis.model.Segment;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by shammyz on 9/30/16.
+ */
+public class SegmentControllerTest {
+    Context context;
+
+    @Before
+    public void setup() {
+        context = InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void testGetAllSegments(){
+        SegmentController segmentController = SegmentController.getInstance(context);
+        try {
+            segmentController.initControllerSegments();
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(segmentController.getAllSegments().size() > 0);
+    }
+
+    @Test
+    public void testGetSegmentByValidId() throws SegmentException, JSONException{
+        SegmentController segmentController = SegmentController.getInstance(context);
+        segmentController.initControllerSegments();
+
+        Segment segment = segmentController.getSegmentById(3927);
+        assertEquals(segment.getId()+"", "3927");
+    }
+
+    @Test
+    public void testGetSegmentByInvalidId() throws SegmentException, JSONException{
+        SegmentController segmentController = SegmentController.getInstance(context);
+        segmentController.initControllerSegments();
+
+        Segment segment = segmentController.getSegmentById(0);
+        assertNull(segment);
+    }
+
+   
+}
