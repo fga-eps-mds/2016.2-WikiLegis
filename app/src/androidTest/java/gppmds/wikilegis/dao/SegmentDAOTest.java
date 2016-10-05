ad1ec70cfe3018b231e279aa33746f55f2ef3679
rodrigocam <rodrigo.redcode@gmail.com>
2016-10-05 19:10:30 -0300

    Adding tests of insertAllSegments and getSegmentById of SegmentDAO

--- app/src/androidTest/java/gppmds/wikilegis/dao/SegmentDAOTest.java
+++ app/src/androidTest/java/gppmds/wikilegis/dao/SegmentDAOTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import gppmds.wikilegis.exception.SegmentException;
 import gppmds.wikilegis.model.Segment;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
@@ -42,5 +43,41 @@ public class SegmentDAOTest {
         } catch (SegmentException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void insertAllSegmentsTest(){
+        SegmentDAO segmentDAO = SegmentDAO.getInstance(context);
+        List<Segment> segmentList = new ArrayList<>();
+        try {
+            Segment segment1 = new Segment(1, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+            Segment segment2 = new Segment(2, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+            Segment segment3 = new Segment(3, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+            Segment segment4 = new Segment(4, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+            Segment segment5 = new Segment(5, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+
+            segmentList.add(segment1);
+            segmentList.add(segment2);
+            segmentList.add(segment3);
+            segmentList.add(segment4);
+            segmentList.add(segment5);
+
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        }
+        assertTrue(segmentDAO.insertAllSegments(segmentList));
+    }
+
+    @Test
+    public void getSegmentByIdTest(){
+        SegmentDAO segmentDAO = SegmentDAO.getInstance(context);
+        Segment segment2 = null;
+        try {
+            Segment segment = new Segment(1, 2, 8, true, 55, 10, 1, 6, "blablabla", "13/12/2006");
+            segment2 = segmentDAO.getSegmentById(1);
+        } catch (SegmentException e) {
+            e.printStackTrace();
+        }
+        assertEquals(segment2.getDate(), "13/12/2006");
     }
 }
