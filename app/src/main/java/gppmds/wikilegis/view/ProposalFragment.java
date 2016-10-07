592f6fd9914f97f49dd30923744b6a1086ea702f
josutk <josuetk63@gmail.com>
2016-10-07 14:26:43 -0300

    Creation save button for suggest proposal

--- app/src/main/java/gppmds/wikilegis/view/ProposalFragment.java
+++ app/src/main/java/gppmds/wikilegis/view/ProposalFragment.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import gppmds.wikilegis.R;
 
 
-public class ProposalFragment extends Fragment {
+public class ProposalFragment extends Fragment implements View.OnClickListener{
 
 
-
+    private Button saveButton;
+    private View view;
     public ProposalFragment() {
         // Required empty public constructor
     }
@@ -23,10 +26,14 @@ public class ProposalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_proposal, container, false);
+        View view = inflater.inflate(R.layout.fragment_proposal, container, false);
+        saveButton = (Button)view.findViewById(R.id.save);
+        saveButton.setOnClickListener(this);
+        return view;
     }
+    @Override
+    public void onClick(View view) {
+        getActivity().getFragmentManager().popBackStack();
 
-
-
-
+    }
 }
