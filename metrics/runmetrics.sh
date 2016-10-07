7c0f720c50cc6500ed9f0719354a76a581d63216
Pedro Ivo <andradepedroivo@gmail.com>
2016-10-07 11:29:07 -0300

    Adding log folder verification and some logs to Script

--- metrics/runmetrics.sh
+++ metrics/runmetrics.sh
@@ -4,6 +4,11 @@
 # Working directories
 javaProjectFolderPath='app/src/main/java/'
 logsPath='metrics/logs'
+logsFolder='logs/'
+
+if [ ! -d "$logsFolder" ]; then
+   mkdir logs
+fi
 
 # Run analizo metrics
 cd ..
@@ -13,34 +18,52 @@ cd metrics
 # Erases previous filtered log, if created before.
 > logs/filtered.log
 
-#analizo javaDirectory > logs/general.log;
+evaluateAcc () {
+   grep acc_mean logs/general.log >> logs/filtered.log;
+   grep acc_quantile_upper logs/general.log >> logs/filtered.log;
+   grep acc_quantile_ninety_five logs/general.log >> logs/filtered.log;
+}
+
+evaluateAccm () {
+   grep accm_mean logs/general.log >> logs/filtered.log;
+   grep accm_quantile_upper logs/general.log >> logs/filtered.log;
+   grep accm_quantile_ninety_five logs/general.log >> logs/filtered.log;
+}
+
+evaluateAmloc () {
+   grep amloc_mean logs/general.log >> logs/filtered.log;
+   grep amloc_quantile_upper logs/general.log >> logs/filtered.log;
+   grep amloc_quantile_ninety_five logs/general.log >> logs/filtered.log;
+}
+
+evaluateLcom () {
+   grep lcom4_mean logs/general.log >> logs/filtered.log;
+   grep lcom4_quantile_upper logs/general.log >> logs/filtered.log;
+   grep lcom4_quantile_ninety_five logs/general.log >> logs/filtered.log;
+}
 
 # Append ACC metrics in file
-grep acc_mean logs/general.log >> logs/filtered.log;
-grep acc_quantile_upper logs/general.log >> logs/filtered.log;
-grep acc_quantile_ninety_five logs/general.log >> logs/filtered.log;
+echo 'Calculating ACC...'
+evaluateAcc
 
 # Append ACCM metrics in file
-grep accm_mean logs/general.log >> logs/filtered.log;
-grep accm_quantile_upper logs/general.log >> logs/filtered.log;
-grep accm_quantile_ninety_five logs/general.log >> logs/filtered.log;
+echo 'Calculating ACCM...'
+evaluateAccm
 
 # Append AMLOC metrics in file
-grep amloc_mean logs/general.log >> logs/filtered.log;
-grep amloc_quantile_upper logs/general.log >> logs/filtered.log;
-grep amloc_quantile_ninety_five logs/general.log >> logs/filtered.log;
+echo 'Calculating AMLOC...'
+evaluateAmloc
 
 # Append LCOM4 metrics in file
-grep lcom4_mean logs/general.log >> logs/filtered.log;
-grep lcom4_quantile_upper logs/general.log >> logs/filtered.log;
-grep lcom4_quantile_ninety_five logs/general.log >> logs/filtered.log;
+echo 'Calculating LCOM4...'
+evaluateLcom
 
-echo 'DONE!\n'
+echo '[OK]'
+echo ''
+echo ''
 
 # Print filtered file on terminal
-cd logs/
-cat filtered.log
-
-cd ..
-
+echo '                        ### METRICS ###'
+echo ''
+cat logs/filtered.log
 
