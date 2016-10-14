e29d740cfc761044d4f3e348ebe97a446635958d
Thiago Nogueira <thiagonf10@gmail.com>
2016-10-13 22:46:49 -0300

    Creating dialog to comments
    Signed-off-by: Thiago Nogueira <thiagonf10@gmail.com>
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>

--- app/src/main/java/gppmds/wikilegis/view/RecyclerViewAdapterContent.java
+++ app/src/main/java/gppmds/wikilegis/view/RecyclerViewAdapterContent.java
@@ -1,17 +1,29 @@
 package gppmds.wikilegis.view;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.Image;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import gppmds.wikilegis.R;
+import gppmds.wikilegis.controller.SegmentController;
 import gppmds.wikilegis.controller.VotesController;
+import gppmds.wikilegis.exception.SegmentException;
 import gppmds.wikilegis.exception.VotesException;
 import gppmds.wikilegis.model.Segment;
 
@@ -24,13 +36,50 @@ public class RecyclerViewAdapterContent extends RecyclerView.Adapter<RecyclerVie
         TextView proposals;
         TextView likes;
         TextView dislikes;
+        ImageView commentSegment;
 
         ContentViewHolder(final View itemView) {
             super(itemView);
-            cardView = (CardView) itemView.findViewById(R.id.card_view);
+            cardView = (CardView) itemView.findViewById(R.id.frameCardViewSegment);
             proposals = (TextView) itemView.findViewById(R.id.textViewSegment);
             likes = (TextView) itemView.findViewById(R.id.textViewNumberLikeCard);
             dislikes = (TextView) itemView.findViewById(R.id.textViewNumberDislikeCard);
+            commentSegment = (ImageView) itemView.findViewById(R.id.imageViewProposalCard);
+
+            commentSegment.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    openDialog(view.getContext());
+                }
+            });
+        }
+
+        private void openDialog(Context context){
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View inputView = inflater.inflate(R.layout.fragment_comment, null);
+
+            AlertDialog.Builder alertDialogProposalBuilder = new AlertDialog.Builder(context);
+            alertDialogProposalBuilder.setView(inputView);
+
+            alertDialogProposalBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Enviar", new DialogInterface.OnClickListener(){
+
+                        public void onClick(DialogInterface dialogBox, int id){
+                            //Colocar o que deve ser feito quando o usuario digitar o comentario
+                        }
+                    })
+
+                    .setNegativeButton("Cancelar",
+                            new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialogBox, int id){
+                                    dialogBox.cancel();
+                                }
+                            });
+
+            AlertDialog alertDialogProposal = alertDialogProposalBuilder.create();
+            alertDialogProposal.show();
         }
     }
 
@@ -48,6 +97,7 @@ public class RecyclerViewAdapterContent extends RecyclerView.Adapter<RecyclerVie
 
     @Override
     public void onBindViewHolder(final ContentViewHolder holder, final int position) {
+        holder.cardView.setTag(R.id.idSegment, listSegment.get(position).getId());
         holder.proposals.setText(listSegment.get(position).getContent());
         try {
             holder.likes.setText(VotesController.getLikesOfSegment(listSegment.get(position).getId()).toString());
