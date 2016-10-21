524304c353e9d22a0eb4b747606592cf4743a931
josutk <josuetk63@gmail.comgetActivity()>
2016-10-20 18:17:11 -0600

    Testing proposal logged out and empty

--- app/src/androidTest/java/gppmds/wikilegis/view/CreateSuggestProposalTest.java
+++ app/src/androidTest/java/gppmds/wikilegis/view/CreateSuggestProposalTest.java
@@ -1,6 +1,8 @@
 package gppmds.wikilegis.view;
 
 import android.app.Activity;
+import android.preference.PreferenceManager;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.WindowManager;
@@ -10,25 +12,31 @@ import org.junit.Test;
 
 import gppmds.wikilegis.R;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by josue on 10/20/16.
  */
 public class CreateSuggestProposalTest extends ActivityInstrumentationTestCase2<LoadingActivity> {
 
-    public CreateSuggestProposalTest(Class<LoadingActivity> activityClass) {
-        super(activityClass);
+    Activity activityOnTest;
+
+    public CreateSuggestProposalTest() {
+        super(LoadingActivity.class);
 
     }
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        final Activity activityOnTest = getActivity();
+        activityOnTest = getActivity();
 
         Runnable wakeUpDevice = new Runnable() {
             public void run() {
@@ -40,8 +48,20 @@ public class CreateSuggestProposalTest extends ActivityInstrumentationTestCase2<
         activityOnTest.runOnUiThread(wakeUpDevice);
     }
 
-    public void test(){
+    public void testSuggestProposalLoggedOut(){
 
+        Boolean isLoggedIn = PreferenceManager.getDefaultSharedPreferences
+                (activityOnTest.getBaseContext()).getBoolean("IsLoggedIn", false);
+
+        if(isLoggedIn){
+            onView(withId(R.id.action_profile)).perform(click());
+            onView(withText("Sair")).perform(click());
+        }
+
+        closeSoftKeyboard();
+        onView(withText("Visitante")).perform(click());
+        onView(withId(R.id.recycler_view_open))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.recycler_viewBill))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.recycler_viewSegment))
@@ -49,6 +69,35 @@ public class CreateSuggestProposalTest extends ActivityInstrumentationTestCase2<
         onView(withId(R.id.floatingButton))
                 .perform((click()));
         onView(withId(R.id.emailLoginField)).check(matches(isDisplayed()));
+
+    }
+
+    public void testEmptyProposalSuggestion(){
+
+        closeSoftKeyboard();
+
+        Boolean isLoggedIn = PreferenceManager.getDefaultSharedPreferences
+                (activityOnTest.getBaseContext()).getBoolean("IsLoggedIn", false);
+
+        if(!isLoggedIn){
+            onView(withId(R.id.emailLoginField)).perform(typeText("cizabelacristina@gmail.com"));
+            closeSoftKeyboard();
+            onView(withId(R.id.passwordLoginField)).perform(typeText("iza3bel"));
+            closeSoftKeyboard();
+            onView(withId(R.id.loginButton)).perform(click());
+        }
+
+        onView(withId(R.id.recycler_view_open))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.recycler_viewBill))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.recycler_viewSegment))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.floatingButton))
+                .perform((click()));
+        onView(withId(R.id.saveSuggestion)).perform(click());
+        onView(withId(R.id.suggestionEditText)).check(matches(hasErrorText(getActivity()
+                .getApplicationContext().getResources().getString(R.string.empty_segment))));
 
     }
 }
