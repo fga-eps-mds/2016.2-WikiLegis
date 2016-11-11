16899328be6f412c6ac6f659b354a9036dea3b00
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2016-11-11 14:02:40 -0200

    Fixing some tests, creating testShareIntent test

--- app/src/androidTest/java/gppmds/wikilegis/view/ViewBillFragmentTest.java
+++ app/src/androidTest/java/gppmds/wikilegis/view/ViewBillFragmentTest.java
@@ -1,16 +1,20 @@
 package gppmds.wikilegis.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 import android.view.WindowManager;
 
+import org.hamcrest.Matcher;
 import org.json.JSONException;
 import org.junit.Before;
+import org.junit.Rule;
 
 import gppmds.wikilegis.R;
 import gppmds.wikilegis.controller.SegmentController;
@@ -28,15 +32,19 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
-public class ViewBillFragmentTest extends ActivityInstrumentationTestCase2<LoadingActivity> {
-
+public class ViewBillFragmentTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
     public ViewBillFragmentTest(){
-        super(LoadingActivity.class);
+        super(LoginActivity.class);
     }
 
     public void setUp() throws Exception {
@@ -92,4 +100,34 @@ public class ViewBillFragmentTest extends ActivityInstrumentationTestCase2<Loadi
         onView(withId(R.id.imageViewLike)).check(matches(isDisplayed()));
 
     }*/
+    //FIXME: Fazer esse teste passar!
+
+    @Rule
+    public IntentsTestRule<LoginActivity> mActivityRule = new IntentsTestRule<>(LoginActivity.class,
+            true, false);
+
+    public static Matcher<Intent> chooser(Matcher<Intent> matcher) {
+        return allOf(hasAction(Intent.ACTION_CHOOSER), hasExtra(is(Intent.EXTRA_INTENT), matcher));
+    }
+
+    public void testShareIntent () throws InterruptedException {
+        onView(withText("Visitante")).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.recycler_view_open))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.action_share_deslogged)).perform(click());
+        Thread.sleep(1000);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "this is my auth token");
+        mActivityRule.launchActivity(intent);
+
+        intended(chooser(allOf(
+                hasAction(Intent.ACTION_SEND),
+                hasExtra(Intent.EXTRA_TEXT, "Expected url")
+        )));
+    }
+
 }
